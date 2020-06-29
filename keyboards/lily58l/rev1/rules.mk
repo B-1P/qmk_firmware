30b6942d931ac522057525c1db39d55f64f153e6
Georg <georg.a.friedrich@gmail.com>
2020-06-30 01:05:57 +0200

    Fixed keyboard def of lily58l

--- keyboards/lily58l/rev1/rules.mk
+++ keyboards/lily58l/rev1/rules.mk
@@ -1,9 +1,5 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # ENables the use of one or more encoders
-<<<<<<< HEAD
 # RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
 
 SRC += led_config.c
-=======
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB light
->>>>>>> my_keymap
